12237554cea5c094c90d03cc90ff033b04fda70f
Sunilsangfroid <sunilsangfroid@gmail.com>
2025-03-23 11:20:17 +0530

    Add runnable parallel implementation for generating social media posts using Google Generative AI

---
+++ PART_II/runnable_parallel.py
@@ -0,0 +1,37 @@
+from langchain_openai import ChatOpenAI
+import os
+from langchain_core.prompts import PromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain.schema.runnable import RunnableSequence, RunnableParallel
+
+load_dotenv()
+
+google_api_key = os.getenv("GOOGLE_API_KEY")
+if not google_api_key:
+    raise ValueError("GOOGLE_API_KEY is not set in the environment variables")
+
+model = ChatGoogleGenerativeAI(model='gemini-1.5-pro', api_key=google_api_key)
+
+
+prompt1 = PromptTemplate(
+    template ='Generate a tweet about {topic}',
+    input_variables=['topic'],
+)
+
+prompt2 = PromptTemplate(
+    template = 'Generate a linkedin post about {topic}',
+    input_variables=['topic'],
+)
+
+parser = StrOutputParser()
+
+parallel_chain = RunnableParallel({
+    'tweet': RunnableSequence(prompt1, model, parser),
+    'linkedin': RunnableSequence(prompt2, model, parser)
+})
+
+result = parallel_chain.invoke({'topic':'AI'})
+
+print(result)
